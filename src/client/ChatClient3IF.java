3cac912bc9f5cd126c82d7d6eb2b42b5e4639779
Saulo Cavalcante <61660671+SauloCav@users.noreply.github.com>
2022-11-15 09:07:21 -0300

    Update ChatClient3IF.java

--- src/client/ChatClient3IF.java
+++ src/client/ChatClient3IF.java
@@ -4,9 +4,6 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ChatClient3IF extends Remote{
-
 	public void messageFromServer(String message) throws RemoteException;
-
 	public void updateUserList(String[] currentUsers) throws RemoteException;
-	
 }
